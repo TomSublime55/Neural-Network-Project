b7156f3dfcbb3284418975548415975ee4a75199
TomSublime55 <20tryan@CMS.tela.org.uk>
2026-03-09 13:26:08 +0000

    Adding the Trainer

---
+++ Trainer.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+file = np.load('array.npy')
+data = torch.tensor(file)
+print(data)
+print(data.size())
+size = data.size()
+
+class Model(nn.Module):
+  def __init__(self, layerSize = [128, 64, 32, 16]):
+    super(Model, self).__init__()
+    layers = []
+    layers.append(nn.Flatten())
+    c = size[0] * size[1]
+    for s in range(len(layerSize) - 1):
+      layers.append(nn.Linear(c, s))
+      layers.append(nn.ReLU())
+      c = s
+    layers.append(nn.Linear(c, 1))
+    self.model = nn.Sequential(*layers)
+
+  def forward(self, x):
+    return self.model(x)
+
+model = Model()
+
+loss = nn.CrossEntropyLoss()
+optimiser = torch.optim.SGD(model.parameters(), lr=0.001)
